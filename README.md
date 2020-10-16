475f8ab32f79cbe9232180814cf54708dcf13cfe
Eliza Kennelly <elizakennelly@gmail.com>
2020-10-15 21:18:17 -0500

    created md function to write to a readme file

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# ReadMeGenerator
+
+    # hello
+    ### DESCRIPTION
+    tehisfdla;
+    ### TABLE OF CONTENTS
+    *[Installation](#installation)
+    *[Usage](#usage)
+    ### Installation Instructions
+    ladooe thigaj;fdas
+    ### Usage Information
+    use things
+    ### Contribution Guidelines
+    contribute plz
+    ### Test Instructions
+    test it all
+    ### License
+    MIT
+    ![license badge](https://img.shields.io/badge/license-MIT-ff69b4)
+
+    
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-const octokit=require("@octokit/core")
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
 
-// const licenses = []
+// badge URL https://img.shields.io/badge/license-MIT-ff69b4
+//github profile link 
+//email address 
 //create array that links to corresponding sections
 // const tableOfContents = ["Description", "Installation", "Usage", "Contributing", "Tests"]
 function userInput(){
@@ -43,15 +44,52 @@ function userInput(){
 
         },
         {
-            type: "checkbox",
+            type: "list",
             message: "Licensing",
             name: "license",
-            choices: await octokit.request('GET /licenses')
-        },
-        console.log("checkbox")
+            choices: ["MIT", "License2", "license4"]
+        }
     ])
 }
-userInput();
-// function generateHTML(answers){
-//     const
-// }
+
+function generateMD(answers) {
+    // Need project title, clickable TOC items, description, installation, usage, 
+    // contributing, tests, license/badge, questions section forlink to github, email 
+    return `
+    # ${answers.title}
+    ### DESCRIPTION
+    ${answers.description}
+    ### TABLE OF CONTENTS
+    *[Installation](#installation)
+    *[Usage](#usage)
+    ### Installation Instructions
+    ${answers.install}
+    ### Usage Information
+    ${answers.usageInfo}
+    ### Contribution Guidelines
+    ${answers.contrib}
+    ### Test Instructions
+    ${answers.test}
+    ### License
+    ${answers.license}
+    ![license badge](https://img.shields.io/badge/license-${answers.license}-ff69b4)
+
+    `
+}
+async function init() {
+    console.log("hi")
+    try {
+      const answers = await userInput();
+  
+      const readme = generateMD(answers);
+  
+      await writeFileAsync("README.md", readme);
+  
+      console.log("Successfully wrote to readme.md");
+    } catch(err) {
+      console.log(err);
+    }
+  }
+  
+  init();
+
